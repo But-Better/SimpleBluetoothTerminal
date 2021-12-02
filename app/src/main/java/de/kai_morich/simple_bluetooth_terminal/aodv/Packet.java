d16fa18cd1b08a52bf651b0f90beb2bd60b2bd26
kev <kevHTW@gmx.de>
2021-12-02 10:31:36 +0100

    Change type to base64

--- app/src/main/java/de/kai_morich/simple_bluetooth_terminal/aodv/Packet.java
+++ app/src/main/java/de/kai_morich/simple_bluetooth_terminal/aodv/Packet.java
@@ -1,8 +1,10 @@
 package de.kai_morich.simple_bluetooth_terminal.aodv;
 
+import java.util.Base64;
+
 public abstract class Packet {
 
     public abstract Object ToBase64(Object o);
-    public abstract String ToPlainText(Object o);
+    public abstract String ToPlainText(Base64 base64);
 
 }
