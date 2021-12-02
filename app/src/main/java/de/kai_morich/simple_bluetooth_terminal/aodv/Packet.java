a0c212a3f761a882fce84595256b813200db5011
kev <kevHTW@gmx.de>
2021-12-02 10:41:16 +0100

    Update Abstract Packet.java

--- app/src/main/java/de/kai_morich/simple_bluetooth_terminal/aodv/Packet.java
+++ app/src/main/java/de/kai_morich/simple_bluetooth_terminal/aodv/Packet.java
@@ -4,7 +4,28 @@ import java.util.Base64;
 
 public abstract class Packet {
 
+    private int hopAddress;
+    private int prevHopAddress;
+
     public abstract Base64 ToBase64(Object o);
+
     public abstract String ToPlainText(Base64 base64);
 
+    public abstract void addPath(Packet packet);
+
+    public int getHopAddress() {
+        return hopAddress;
+    }
+
+    public void setHopAddress(int hopAddress) {
+        this.hopAddress = hopAddress;
+    }
+
+    public int getPrevHopAddress() {
+        return prevHopAddress;
+    }
+
+    public void setPrevHopAddress(int prevHopAddress) {
+        this.prevHopAddress = prevHopAddress;
+    }
 }
