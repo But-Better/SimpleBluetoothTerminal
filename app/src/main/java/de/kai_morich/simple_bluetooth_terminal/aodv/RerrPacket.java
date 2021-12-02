8d0e09dd6075aac7f508682570f7ecaf624b1e61
kev <kevHTW@gmx.de>
2021-12-02 10:49:47 +0100

    impl abstract

--- app/src/main/java/de/kai_morich/simple_bluetooth_terminal/aodv/RerrPacket.java
+++ app/src/main/java/de/kai_morich/simple_bluetooth_terminal/aodv/RerrPacket.java
@@ -1,4 +1,15 @@
 package de.kai_morich.simple_bluetooth_terminal.aodv;
 
+import java.util.Base64;
+
 public class RerrPacket extends Packet{
+    @Override
+    public Base64 ToBase64(Object o) {
+        return null;
+    }
+
+    @Override
+    public String ToPlainText(Base64 base64) {
+        return null;
+    }
 }
