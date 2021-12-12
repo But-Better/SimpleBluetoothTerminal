3034ef92de19f3b50fd4789d67e3d66427351caf
Mathis <mathis.schott@gmail.com>
2021-12-12 16:58:15 +0100

    add messageType detector for incoming messages

--- app/src/main/java/de/kai_morich/simple_bluetooth_terminal/aodv/Handle/StreamDataHandle.java
+++ app/src/main/java/de/kai_morich/simple_bluetooth_terminal/aodv/Handle/StreamDataHandle.java
@@ -4,6 +4,8 @@ import java.util.Base64;
 
 public class StreamDataHandle {
 
+
+
     public String readDataTraffic(byte[] msg) {
         return analysePacket(Base64.getEncoder().encodeToString(msg));
     }
@@ -15,4 +17,17 @@ public class StreamDataHandle {
         }
         return null;
     }
+
+    /**
+     * returns the type of the message as an integer
+     * @param msg that is recieved
+     * @return 0 for RREQ, 1 for RREP, 2 for RERR, 3 for MSG, 4 for ACK
+     */
+    public int getPacketType(byte[] msg){
+        if(msg[0]%2==1){
+            return ((msg[0] - 1) / (int) Math.pow(2, 4));
+        }
+        return ((msg[0]) / (int) Math.pow(2, 4));
+    }
+
 }
