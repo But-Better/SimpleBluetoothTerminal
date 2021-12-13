13fe67305bc1aade2fd73b3da994f3dc5a0cc987
kev <kevHTW@gmx.de>
2021-12-13 16:30:33 +0100

    refactor lib import

--- app/src/main/java/de/kai_morich/simple_bluetooth_terminal/SerialSocket.java
+++ app/src/main/java/de/kai_morich/simple_bluetooth_terminal/SerialSocket.java
@@ -17,6 +17,7 @@ import java.util.concurrent.Executors;
 
 import aodv.Handle.StreamData;
 
+
 class SerialSocket implements Runnable {
 
     private static final UUID BLUETOOTH_SPP = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
